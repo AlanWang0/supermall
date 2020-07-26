e0db512ba827c0b1bcd6a75dec537049516e7c73
AlanWang0 <13128667160@163.com>
2020-07-27 05:22:57 +0800

    dist

---
+++ dist/js/chunk-2d2163ab.76e7c3e6.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d2163ab"],{c228:function(n,e,t){"use strict";t.r(e);var a=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("h2",[n._v("购物车车")])},c=[],u={name:"Cart"},l=u,r=t("2877"),s=Object(r["a"])(l,a,c,!1,null,null,null);e["default"]=s.exports}}]);
+//# sourceMappingURL=chunk-2d2163ab.76e7c3e6.js.map
